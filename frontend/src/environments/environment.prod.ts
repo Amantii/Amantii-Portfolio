7db7255e662b364883bc343aba9779a235d7bb79
Amantii <amantiisamson262@gmail.com>
2026-04-07 22:59:25 -0400

    Fix environment files and add emailjs credentials

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {                                                                                                                              
   production: true,         
-  apiBaseUrl: ''  // your prod URL when you haveit                                                                                                  
+  apiBaseUrl: '',
+  emailJs: {
+    serviceId: 'service_r5ndyup',
+    templateId: 'template_9636o2c',
+    publicKey: 'XL4uBZDJjeI5R2J2P'
+  }  // your prod URL when you haveit                                                                                                  
 };
