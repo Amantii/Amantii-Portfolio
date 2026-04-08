8ce8dd046d796f6a7393d16a0a5dbf74ca1ecc79
Amantii <amantiisamson262@gmail.com>
2026-04-07 23:03:03 -0400

    Track environment.ts for Vercel build

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {                                                                                                                              
+ production:false,                                                                                                            
+ apiBaseUrl:'http://localhost:8080',
+ emailJs: {
+  serviceId: 'service_r5ndyup',
+  templateId: 'template_9636o2c',
+  publicKey: 'XL4uBZDJjeI5R2J2P'
+ }
+}; 
